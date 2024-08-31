6bf0c7fe6fbc12bb8ce6a7e844da464eb037c956
Jarinya008 <jarinyawhan@gmail.com>
2024-09-01 06:02:57 +0700

    e

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
 const mysql = require('mysql2');
 
 const app = express();
+app.use(express.json());
 const PORT = 3306;
 
 // MySQL database connection configuration
