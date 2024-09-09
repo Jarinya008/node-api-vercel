aea0e7fb1b1456d0d77eb0b6cfefa4f55706e60e
aintira11 <aintrlala@gmail.com>
2024-09-09 15:23:49 +0700

    kk

--- index.js
+++ index.js
@@ -257,12 +257,13 @@ app.get('/searchLotto', (req, res) => {
             console.error('Error searching for lotto number:', err);
             res.status(500).send('An error occurred while searching for the lotto number.');
         } else if (results.length > 0) {
-            res.json({ data: results });
+            res.json(results); // ส่งผลลัพธ์ JSON 
         } else {
-            res.json({ message: 'No matching lotto numbers found.' });
+            res.json([]); // ส่ง array ว่างในกรณีที่ไม่พบข้อมูล
         }
     });
 });
+
 
 //เลขลงตะกร้า
 app.post('/Add_to_basket', (req, res) => {
@@ -291,7 +292,7 @@ app.get('/My_basket/:member_id', (req, res) => {
             console.error('Error searching for basket:', err);
             res.status(500).send('An error occurred while searching for the basket.');
         } else if (results.length > 0) {
-            res.json({ message: 'All Lotto numbers in the basket.', data: results });
+            res.json(results);
         } else {
             res.json({ message: 'Empty basket.' });
         }
@@ -308,7 +309,7 @@ app.get('/My_buyLotto/:member_id', (req, res) => {
             console.error('Error searching for basket:', err);
             res.status(500).send('An error occurred while searching for the basket.');
         } else if (results.length > 0) {
-            res.json({ message: 'All Lotto numbers in the basket.', data: results });
+            res.json(results);
         } else {
             res.json({ message: 'Empty basket.' });
         }
@@ -460,11 +461,6 @@ app.get('/get_Lotto_status1', (req, res) => {
     });
 });
 
-
-//ออกรางวัล(ขายแล้ว)
-app.post('reward_buy',(req,res) => {
-
-});
 
 
 
